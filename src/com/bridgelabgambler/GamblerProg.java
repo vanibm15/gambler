e51eaf8e345f5bbf2eb3972c0c1614a6d2a1d9d7
vanibm15 <vanibm15@gmail.com>
2022-04-26 14:09:55 +0530

    added:gamblers result (win/loose)

--- src/com/bridgelabgambler/GamblerProg.java
+++ src/com/bridgelabgambler/GamblerProg.java
@@ -1,20 +1,35 @@
 package com.bridgelabgambler;
 
-public class GamblerProg
-{
+import java.util.Random;
+import java.util.Scanner;
 
-        static int stake=100;
-        static int bet = 1;
+public class GamblerProg {
 
-        static void method1() {
-            System.out.println("the glambler had a stake:" +"" +stake);
-            System.out.println("the glambler will bet:" +bet);
+    static int stake = 100;
+    static int bet = 1;
+    static Random betnum = new Random();
+
+    static void method1() {
+        System.out.println("the glambler had a stake:" + "" + stake);
+        System.out.println("the glambler will bet:" + bet);
+    }
+
+    static void method2() {
+        int checknum = betnum.nextInt(2);
+        if (checknum == 1) {
+            System.out.println("he wins");
+
+        } else {
+            System.out.println("he lose");
+
+
         }
-
-
-        public static void main(String[] args) {
+    }
+        public static void main (String[]args){
             method1();
+            method2();
         }
 
-}
+    }
+
 
