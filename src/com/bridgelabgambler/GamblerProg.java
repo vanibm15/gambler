5357ea6a7d4334a5d863031117a39bbfb6954626
vanibm15 <vanibm15@gmail.com>
2022-04-26 13:54:21 +0530

    added:welcome message

---
+++ src/com/bridgelabgambler/GamblerProg.java
@@ -0,0 +1,9 @@
+package com.bridgelabgambler;
+
+public class GamblerProg
+{
+    public static void main(String[] args) {
+        System.out.println("welcome to gambler stimulation" );
+    }
+}
+
